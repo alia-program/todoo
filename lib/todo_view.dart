23a64ae9436f7ffbf1ce1d7ddca9fa077a610219
alia-program <aliaillust.program@gmail.com>
2024-10-20 22:05:09 +0900

    削除＆追加機能(id)

--- lib/todo_view.dart
+++ lib/todo_view.dart
@@ -18,11 +18,11 @@ class ItemData {
 }
 
 class _TodoViewState extends State<TodoView> {
-  //作成した番号を受け取ったらintにいれる
-  final List<ItemData> _items = List<ItemData>.generate(
-      3, (index) => ItemData(index, '', TextEditingController(text: "")));
+  final _items = <ItemData>[];
+  //  //.generate(3, (index) => ItemData(index, '', TextEditingController(text: "")));
   String content = "";
   int index = 0;
+  int id = 0;
 
   @override
   void dispose() {
@@ -33,90 +33,34 @@ class _TodoViewState extends State<TodoView> {
   @override
   void initState() {
     super.initState();
-
     _items[index]._controller = TextEditingController(text: _items[index].text);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ReorderableListView(
-          onReorder: (oldIndex, newIndex) {
-            setState(() {
-              if (oldIndex < newIndex) {
-                newIndex--;
-              }
-              var item = _items.removeAt(oldIndex);
-              _items.insert(newIndex, item);
-            });
-          },
-          //リストのアイテムたちを生成
-          children: <Widget>[
-            for (var item in _items)
-              Slidable(
-                key: ValueKey(item),
-                endActionPane: ActionPane(
-                    extentRatio: 0.2,
-                    motion: const StretchMotion(),
-                    children: [
-                      SlidableAction(
-                        label: '削除',
-                        backgroundColor: Colors.red,
-                        icon: Icons.delete,
-                        onPressed: (context) {
-                          _removeItem(item);
-                        },
-                      ),
-                    ]),
-                child: Row(
-                  key: ValueKey(item),
-                  //複数のviewを配置
-                  children: <Widget>[
-                    //Expandedは最大限広げる
-                    Expanded(
-                      //Containerは色・サイズ・childを指定できる
-                      child: Container(
-                        //線の表示は同じ箱に入れて同じ範囲適用
-                        decoration: const BoxDecoration(
-                          border: Border(
-                            bottom: BorderSide(
-                                color: Color.fromARGB(255, 173, 173, 173)),
-                          ),
-                        ),
-                        //入力画面
-                        child: EditableText(
-                          controller: item._controller,
-                          onChanged: (newText) {
-                            setState(() {
-                              item.text = newText;
-                              index = _items.indexOf(item);
-                            });
-                          },
-                          focusNode: FocusNode(),
-                          style: const TextStyle(
-                            color: Color.fromARGB(255, 150, 150, 150),
-                            fontSize: 40,
-                          ),
-                          cursorColor: Colors.black,
-                          backgroundCursorColor: Colors.black,
-                        ),
-                      ),
-                    ),
-
-                    //padding付きのアイコン
-                    const Padding(
-                      padding: EdgeInsets.all(15),
-                      child: Icon(
-                        Icons.reorder,
-                        size: 40,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-          ],
-        ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              OutlinedButton(
+                  onPressed: () {
+                    setState(() {
+                      _addItem();
+                    });
+                  },
+                  child: const Text('追加')),
+              OutlinedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('削除')),
+            ],
+          ),
+          Expanded(
+            child: _addListView(),
+          )
+        ],
       ),
     );
   }
@@ -125,5 +69,88 @@ class _TodoViewState extends State<TodoView> {
     setState(() {
       _items.remove(item);
     });
+  }
+
+  void _addItem() {
+    _items.add(ItemData(id++, '', TextEditingController()));
+  }
+
+  ReorderableListView _addListView() {
+    return ReorderableListView(
+      onReorder: (oldIndex, newIndex) {
+        setState(() {
+          if (oldIndex < newIndex) {
+            newIndex--;
+          }
+          var item = _items.removeAt(oldIndex);
+          _items.insert(newIndex, item);
+        });
+      },
+      //リストのアイテムたちを生成
+      children: <Widget>[
+        for (var item in _items)
+          Slidable(
+            key: ValueKey(item),
+            endActionPane: ActionPane(
+                extentRatio: 0.2,
+                motion: const StretchMotion(),
+                children: [
+                  SlidableAction(
+                    label: '削除',
+                    backgroundColor: Colors.red,
+                    icon: Icons.delete,
+                    onPressed: (context) {
+                      _removeItem(item);
+                    },
+                  ),
+                ]),
+            child: Row(
+              key: ValueKey(item),
+              //複数のviewを配置
+              children: <Widget>[
+                //Expandedは最大限広げる
+                Expanded(
+                  //Containerは色・サイズ・childを指定できる
+                  child: Container(
+                    //線の表示は同じ箱に入れて同じ範囲適用
+                    decoration: const BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(
+                            color: Color.fromARGB(255, 173, 173, 173)),
+                      ),
+                    ),
+                    //入力画面
+                    child: EditableText(
+                      controller: item._controller,
+                      onChanged: (newText) {
+                        setState(() {
+                          item.text = newText;
+                          index = _items.indexOf(item);
+                        });
+                      },
+                      focusNode: FocusNode(),
+                      style: const TextStyle(
+                        color: Color.fromARGB(255, 150, 150, 150),
+                        fontSize: 40,
+                      ),
+                      cursorColor: Colors.black,
+                      backgroundCursorColor: Colors.black,
+                    ),
+                  ),
+                ),
+
+                //padding付きのアイコン
+                const Padding(
+                  padding: EdgeInsets.all(15),
+                  child: Icon(
+                    Icons.reorder,
+                    size: 30,
+                  ),
+                )
+              ],
+            ),
+          ),
+      ],
+    );
   }
 }
